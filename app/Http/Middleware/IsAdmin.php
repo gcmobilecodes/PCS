83edd70b959dbf8f5aa580d72b596246206666a4
Nishant-Gorilla <nb.gorilladevelopment@gmail.com>
2022-06-17 17:26:30 +0530

    update

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -17,7 +17,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if(Auth::check() &&Auth::user()-> user_type==2){
+        if(Auth::check() &&Auth::user()-> user_type==3){
             return $next($request);
 
         }
