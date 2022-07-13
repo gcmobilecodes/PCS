a59a0c9db1e9fe1850df8ca886e7ac2b1b008efb
Nishant-Gorilla <nb.gorilladevelopment@gmail.com>
2022-07-13 19:55:26 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,8 @@ Route::get('/dates_history_search',[Checkincheckout_detailController::class, 'lo
 Route::get('/datessearch',[Checkincheckout_detailController::class, 'datepicker'])->name('datepicker');
 Route::get('/dates_history_search',[Checkincheckout_detailController::class, 'login_history_search'])->name('admin.login_history_search');
 Route::get('/datepicker',[Checkincheckout_detailController::class, 'datePickers'])->name('datepickers');
+Route::get('/datepicksser',[Checkincheckout_detailController::class, 'datePickers'])->name('datepickers');
+
 
 Route::get('link',function(){
     Artisan::call('storage:link');
