6d9fac4b7ce9929ab8214e7ae3db44df71d89608
Nishant-Gorilla <nb.gorilladevelopment@gmail.com>
2022-07-18 15:12:29 +0530

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
     // }
     function getCheckinoutDetail()
     {
-        return $this->hasOne(History::class);
+        return $this->hasOne(Checkinckeckout::class);
     }
 
     /**
