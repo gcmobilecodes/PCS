ac6c9eeedf14e3eb54954d50114b2a508af7b465
Nishant-Gorilla <nb.gorilladevelopment@gmail.com>
2022-07-19 20:06:35 +0530

    update

--- database/migrations/2022_07_19_143552_create_history_table.php
+++ database/migrations/2022_07_19_143552_create_history_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->string('checkout_address')->nullable();
              $table->integer('status');
             $table->timestamps();
+            $table->timestamps();
         });
     }
 
